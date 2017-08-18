06a3f2154d0bd587d4df9695dded70cd0c6a29e7
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2017-08-18 18:22:36 +0545

    Adds more UI

---
+++ src/SLM.java
@@ -0,0 +1,12 @@
+import slm.LibraryInterface;
+import slm.DBManager;
+import slm.Librarian;
+import slm.Book;
+import slm.Member;
+
+class SLM{
+	public static void main(String[] args) {
+			DBManager.init_database();
+			LibraryInterface.init_interface();
+	}
+}
